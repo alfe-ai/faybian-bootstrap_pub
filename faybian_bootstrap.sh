3a07c125726bd076651d7b15fcfed6b01d438e7b
whimsy <whimsy@sylph.box>
2025-03-24 21:37:40 -0500

    rearrange

--- faybian_bootstrap.sh
+++ faybian_bootstrap.sh
@@ -34,15 +34,18 @@ mkdir /home/$USER/.alfe.sh/git
 mkdir /home/$USER/.alfe.sh/FaybianScripts
 mkdir /home/$USER/.alfe.sh/FaybianScripts/utils
 
-# Set permissions for ~/.alfe.sh and subdirectories to $USER
-sudo chown -R $USER:$USER /home/$USER/.alfe.sh
-
 print_section_header "Verifying created directories"
 if [ -d "/home/$USER/.alfe.sh" ] && [ -d "/home/$USER/.alfe.sh/git" ] && [ -d "/home/$USER/.alfe.sh/FaybianScripts" ] && [ -d "/home/$USER/.alfe.sh/FaybianScripts/utils" ]; then
   echo "Directories exist."
 else 
   echo "Directories do not exist."
 fi
+
+print_section_header "Download and install textc.sh"
+sudo wget --header="Cache-Control: no-cache" --header="Pragma: no-cache" -O /home/$USER/.bashrc "https://raw.githubusercontent.com/alfe-ai/faybian-scripts/refs/heads/main/filesystemRoot/home/user/bashrc?$(date +%s)"
+
+print_section_header "Set permissions for ~/.alfe.sh and subdirectories to $USER"
+sudo chown -R $USER:$USER /home/$USER/.alfe.sh
 
 print_section_header "Install required packages"
 sudo apt install redshift fastfetch -y
