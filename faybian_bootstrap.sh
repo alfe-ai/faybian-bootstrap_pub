7f7af8faeb61849dc25499dd15191489eca8b9da
whimsy <whimsy@sylph.box>
2025-03-24 21:29:13 -0500

    - Updated  to set the correct permissions for the  directory and its subdirectories to ensure they are owned by the user

--- faybian_bootstrap.sh
+++ faybian_bootstrap.sh
@@ -7,14 +7,9 @@ print_section_header() {
   echo "==============================="
 }
 
-# TODO: Have the script increase display resolution and enable Gnome Dark Style
-
-# Stage 1: Init
 print_section_header "Checking sudo access for $USER"
 
-# Check if the user doesn't already exist in /etc/sudoers
 if ! sudo grep -q "$USER ALL=(ALL:ALL) ALL" /etc/sudoers; then
-  # Grant the user sudo access
   echo "Adding $USER to /etc/sudoers"
   echo "$USER ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
 else
@@ -23,18 +18,18 @@ fi
 
 print_section_header "Overwriting /etc/apt/sources.list"
 
-# Overwrite /etc/apt/sources.list with the file from the repository
-sudo wget --header="Cache-Control: no-cache" --header="Pragma: no-cache" -O /etc/apt/sources.list "https://raw.githubusercontent.com/alfe-ai/faybian-bootstrap_pub/refs/heads/main/fileSystem/etc/apt/sources.list?$(date +%s)" #TODO: sha/pgp verification
+sudo wget --header="Cache-Control: no-cache" --header="Pragma: no-cache" -O /etc/apt/sources.list "https://raw.githubusercontent.com/alfe-ai/faybian-bootstrap_pub/refs/heads/main/fileSystem/etc/apt/sources.list?$(date +%s)"
 
 print_section_header "Updating and upgrading packages"
 
-# Upgrade packages with default Yes to the prompt
 sudo apt update -y && sudo apt upgrade -y
 
-# Stage 2: ALSH
 print_section_header "Creating directory: ~/.alfe.sh and subdirectories."
 mkdir /home/$USER/.alfe.sh
 mkdir /home/$USER/.alfe.sh/git
+
+# Set permissions for ~/.alfe.sh and subdirectories to $USER
+sudo chown -R $USER:$USER /home/$USER/.alfe.sh
 
 print_section_header "Verifying created directories"
 if [ -d "/home/$USER/.alfe.sh" ] && [ -d "/home/$USER/.alfe.sh/git" ]; then
@@ -47,5 +42,4 @@ print_section_header "Install required packages"
 sudo apt install redshift -y
 
 print_section_header "Download and install ALSH"
-sudo wget --header="Cache-Control: no-cache" --header="Pragma: no-cache" -O /home/$USER/.bashrc "https://raw.githubusercontent.com/alfe-ai/faybian-scripts/refs/heads/main/filesystemRoot/home/user/bashrc?$(date +%s)" #TODO: sha/pgp verification
-
+sudo wget --header="Cache-Control: no-cache" --header="Pragma: no-cache" -O /home/$USER/.bashrc "https://raw.githubusercontent.com/alfe-ai/faybian-scripts/refs/heads/main/filesystemRoot/home/user/bashrc?$(date +%s)"
