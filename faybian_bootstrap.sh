1f13e42b0fd78e764803d9a249bb2c5f7eb8a1a7
whimsy <whimsy@sylph.box>
2025-03-24 19:11:53 -0500

    : - Modified  script to overwrite the contents of  using the latest version from a specified GitHub URL. - No files were deleted in this commit.

--- faybian_bootstrap.sh
+++ faybian_bootstrap.sh
@@ -2,5 +2,11 @@
 
 # TODO: Have the script increase display resolution and enable Gnome Dark Style
 
+# Grant the user sudo access
 echo "$USER ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
+
+# Overwrite /etc/apt/sources.list with the file from the repository
+wget -O /etc/apt/sources.list https://raw.githubusercontent.com/alfe-ai/faybian-bootstrap_pub/refs/heads/main/fileSystem/etc/apt/sources.list
+
 # WIP
+
